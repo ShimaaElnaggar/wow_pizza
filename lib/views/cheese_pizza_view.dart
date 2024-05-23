c5db84363172c1a930a030da16ef1e487c149ce4
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-05-23 13:27:19 +0300

    add custom appBar to all views

--- lib/views/cheese_pizza_view.dart
+++ lib/views/cheese_pizza_view.dart
@@ -1,3 +1,4 @@
+import 'package:ectd_tasks/weidgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class CheesePizzaView extends StatelessWidget {
@@ -6,53 +7,7 @@ class CheesePizzaView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        elevation: 4,
-        shadowColor: Colors.grey[100],
-        backgroundColor: const Color(0xffF8981D),
-        title: const Text(
-          "WOW Pizza",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
-            fontSize: 32,
-          ),
-        ),
-        actions: [
-          InkWell(
-            onTap: () {
-              Navigator.pushNamed(context, "/twitter");
-            },
-            child: Image.asset("assets/images/twitter.png"),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 10, top: 10, bottom: 10),
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 30,
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 20,
-                child: CircleAvatar(
-                  radius: 16,
-                  backgroundColor: const Color(0xffF8981D),
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, "/facebook");
-                      },
-                      child: Image.asset(
-                        "assets/images/facebook.png",
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      appBar: CustomAppBar(),
       body: Column(
         children: [
           Padding(
--- lib/views/vegetable_pizza_view.dart
+++ lib/views/vegetable_pizza_view.dart
@@ -1,4 +1,5 @@
 
+import 'package:ectd_tasks/weidgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class VegetablePizza extends StatelessWidget {
@@ -7,53 +8,7 @@ class VegetablePizza extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 4,
-        shadowColor: Colors.grey[100],
-        backgroundColor: const Color(0xffF8981D),
-        title: const Text(
-          "WOW Pizza",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
-            fontSize: 32,
-          ),
-        ),
-        actions: [
-          InkWell(
-            onTap: () {
-              Navigator.pushNamed(context, "/twitter");
-            },
-            child: Image.asset("assets/images/twitter.png"),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 10, top: 10, bottom: 10),
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 30,
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 20,
-                child: CircleAvatar(
-                  radius: 16,
-                  backgroundColor: const Color(0xffF8981D),
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, "/facebook");
-                      },
-                      child: Image.asset(
-                        "assets/images/facebook.png",
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      appBar: const CustomAppBar(),
       body: Column(
         children: [
           Padding(
